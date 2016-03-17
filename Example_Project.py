a11b7be2f8ad8e0b2b6ccfaf51683cd23c31b2ad
RadhaRamachandran <radha.ramachdran@gmail.com>
2016-03-17 12:14:03 -0500

    Scale data by number added

--- Example_Project.py
+++ Example_Project.py
@@ -38,7 +38,7 @@ max_value = max(max_value1,max_value2)
 
 #----------------------------------------#
 
-# Step 3: SCALE DATA
+# Step 3a: SCALE DATA BY COLOR. TO SCALE DATA BY NUMBER, look at step 3b.
 
 color_low = Green
 color_high = Red
@@ -48,24 +48,58 @@ clr_winter = ColorScaler(color_low, color_high, value2, min_value, max_value)
 
 #----------------------------------------#
 
-# Step 4: Display data on LEDs
+# Step 3b : SCALE DATA BY NUMBER OF LEDs
+# Using entire 80 LEDs to scale the summer data alone. 
+# If you are only using the bottom panel, do the command twice - once with summer and once with winter. 
+# Numscaler() returns the last LED to be turned on. 
+# So for the bottom panel, turn on from 0 --> NumLEDs. For the top panel turn on 40 --> numLEDs.
+
+numLED_all = NumScaler(0, 79, value1, min_value, max_value)
+# Bottom panel is LEDs 0,39 for the first two numbers. Top panel is LEDs 40, 79. All LEDs are 0, 79. 
+
+#----------------------------------------#
+
+# Step 4a: Display COLOR data on LEDs
+# This displays data once and stops
+print ('Displaying COLOR data')
+
+Flash(5, 0.5, Purple) ## Flash everytime it starts over
+size = len(clr_summer)
+
+for i in range(size):
+    # Summer on Bottom --> LEDS 0-39. 
+
+    led.fill(clr_summer[i], 0, 39)
+
+    # Winter on TOP --> LEDS 40-79
+    led.fill(clr_winter[i], 40, 79)
+
+    led.update()
+    time.delay(0.5)
+
+
+#----------------------------------------#
+
+# Step 4b: Display NUMBER data on LEDs
+# This displays data continuously
+
+print ('Displaying NUMBER DATA')
+
+color_summer = Yellow
+
+# We are letting the user know how to stop this program
 print ('Press control + C to stop the program')
 
 while (True): ## TO CONTINUOUSLY REPEAT DATA
-
+    
     Flash(5, 0.5, Purple) ## Flash everytime it starts over
-    size = len(numLED_summer)
+    size = len(numLED_all)
 
     for i in range(size):
-        # Summer on Bottom --> LEDS 0-39. 
+        # Summer on all panels --> LEDS 0-79. 
 
-        led.fill(clr_summer[i], 0, 39)
-        
-        # Winter on TOP --> LEDS 40-79
-        led.fill(clr_winter[i], 40, 79)
-        
+        led.fill(color_summer, 0, numLED_all[i])
         led.update()
         time.delay(0.5)
-
 
 
