4dbb7ef7b4a98a897f6b03ab7c711d93fdb84366
Tom Ritchford <tom@swirly.com>
2018-04-03 21:03:11 +0200

    Update LEDsetup.py for BP3.x

--- DEMO/LEDsetup.py
+++ DEMO/LEDsetup.py
@@ -1,21 +1,16 @@
-
-# coding: utf-8
-
 # In[1]:
 
-# The lines reset a driver called spidev, 
+# The lines reset a driver called spidev,
 # that is used to drive the signal sent to the lights from the raspberry pi
 from subprocess import call
 call(["sudo", "chmod", "a+rw", "/dev/spidev0.0"])
 
 # Importing some parts of a 'library' of programs called bibliopixel
-# Bibliopixel has the all the functions we use to light up LEDs 
+# Bibliopixel has the all the functions we use to light up LEDs
 # like led.set(), led.fill() etc
 
-from bibliopixel import *
-from bibliopixel.drivers.LPD8806 import *
-from bibliopixel.drivers.driver_base import *
-from bibliopixel import LEDStrip
+from bibliopixel.layout import Strip
+from bibliopixel.drivers.SPI import LPD8806
 
 # Importing the library that has the function time.sleep()
 
@@ -25,21 +20,19 @@ import time
 # In[2]:
 
 # numLeds is the number of LEDs in our lamp
-# we set the driver to the specific one that drives our LED lights 
+# we set the driver to the specific one that drives our LED lights
 # along with the information about how many lights there are
-# Each color is a set of 3 numbers, 
+# Each color is a set of 3 numbers,
 # ChannelOrder says in what order the 3 colors Red, Green, Blue are organized
 # in our case it is Blue Red Green (BRG)
 
+numLeds = 130
+driver = LPD8806(numLeds, ChannelOrder.BRG)
 
-
-numLeds=130
-driver=DriverLPD8806(numLeds, ChannelOrder.BRG)
-
-# We name our LEDstrips 'led'. 
+# We name our LEDstrips 'led'.
 # We tell the driver that it is a long string of LEDs, not a rectangle or any other shape
 
-led=LEDStrip(driver)
+led = Strip(driver)
 
 
 # In[1]:
@@ -68,4 +61,3 @@ Lime = (0, 255, 75)
 Khaki = (240, 150, 60)
 Maroon = (70, 0, 0)
 Gold = (255, 140, 0)
-
